693d060dff5e13e69ed46753e0e107eeb894d48d
AKM1189 <aungkaungmyat912002@gmail.com>
2025-07-31 16:43:52 +0630

    remove .then

--- src/app.ts
+++ src/app.ts
@@ -18,49 +18,31 @@ declare global {
 }
 
 export const createApp = async () => {
-  return AppDataSource.initialize()
-    .then(async () => {
-      const app = express();
+  await AppDataSource.initialize();
+  const app = express();
 
-      // Update CORS for production
-      const allowedOrigins =
-        process.env.NODE_ENV === "production"
-          ? [process.env.PRODUCTION_FRONTEND_URL]
-          : ["http://localhost:5176"];
+  // Update CORS for production
+  const allowedOrigins =
+    process.env.NODE_ENV === "production"
+      ? [process.env.PRODUCTION_FRONTEND_URL]
+      : ["http://localhost:5176"];
 
-      app.use(
-        cors({
-          origin: allowedOrigins,
-          credentials: true,
-        }),
-      );
+  app.use(
+    cors({
+      origin: allowedOrigins,
+      credentials: true,
+    }),
+  );
 
-      app.use(express.json());
-      app.use(express.urlencoded({ extended: false }));
-      app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 
-      app.use("/api/auth/", authRoutes);
-      app.use("/api/admin/", movieRoutes);
-      app.use("/api/admin/", genreRoutes);
+  app.use("/api/auth/", authRoutes);
+  app.use("/api/admin/", movieRoutes);
+  app.use("/api/admin/", genreRoutes);
 
-      return app;
-
-      //////
-
-      // const user = new User()
-      // user.firstName = "Timber"
-      // user.lastName = "Saw"
-      // user.age = 25
-      // await AppDataSource.manager.save(user)
-      // console.log("Saved a new user with id: " + user.id)
-
-      // console.log("Loading users from the database...")
-      // const users = await AppDataSource.manager.find(User)
-      // console.log("Loaded users: ", users)
-
-      // console.log("Here you can setup and run express / fastify / any other framework.")
-    })
-    .catch((error) => error);
+  return app;
 };
 const localServer = async () => {
   const app = await createApp();
