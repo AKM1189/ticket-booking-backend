47137df88ab2e91a5298d00d9528229f3f2ba5bc
AKM1189 <aungkaungmyat912002@gmail.com>
2025-07-31 14:20:18 +0630

    remove hello

--- api/index.js
+++ api/index.js
@@ -4,10 +4,10 @@ import serverless from "serverless-http";
 let cachedHandler;
 
 export default async function handler(req, res) {
-  res.status(200).json({ message: "Hello from Vercel!" });
   if (!cachedHandler) {
     const app = await createApp();
     cachedHandler = serverless(app);
   }
+  // res.status(200).json({ message: "Hello from Vercel!" });
   return cachedHandler(req, res);
 }
