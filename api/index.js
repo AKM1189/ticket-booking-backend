b363ad406fc7f431009afd83c875a087b272e8bd
AKM1189 <aungkaungmyat912002@gmail.com>
2025-07-31 14:03:06 +0630

    hello added

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import serverless from "serverless-http";
 let cachedHandler;
 
 export default async function handler(req, res) {
+  res.status(200).json({ message: "Hello from Vercel!" });
   if (!cachedHandler) {
     const app = await createApp();
     cachedHandler = serverless(app);
